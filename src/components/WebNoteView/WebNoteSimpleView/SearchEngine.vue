c1ed53bff3751760b19a490b2b7ba4ebe0a8102f
quanting56 <qwerdgt@gmail.com>
2025-07-25 22:07:11 +0800

    feat: 新增WebNoteSimple內容

---
+++ src/components/WebNoteView/WebNoteSimpleView/SearchEngine.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <select v-model="engine">
+      <option value="Google">Google</option>
+      <option value="Yahoo">Yahoo!奇摩</option>
+      <option value="Bing">Bing</option>
+    </select>
+    <button @click="toSearchEngine()">Go!</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const engine = ref("Google");
+
+function toSearchEngine() {
+  const urls = {
+    Google: "https://www.google.com.tw",
+    Yahoo: "https://tw.yahoo.com",
+    Bing: "https://www.bing.com",
+  };
+  window.open(urls[engine.value], "_blank");
+};
+</script>
--- src/views/WebNote/WebNoteSimpleView.vue
+++ src/views/WebNote/WebNoteSimpleView.vue
@@ -25,11 +25,27 @@
         <div class="description" v-if="currentTab.description">
           <p>{{ currentTab.description }}</p>
         </div>
-        <div
-          class="demo"
-          :style="currentTab.demoStyle"
-          v-html="currentTab.demo"></div>
-        <pre><code class="html">{{ currentTab.demo }}</code></pre>
+
+        <div class="demo">
+          <!-- 若有component，就顯示component -->
+          <component v-if="currentTab.component" :is="currentTab.component"></component>
+          <!-- 否則用v-html顯示demo -->
+          <div
+            v-else
+            v-html="currentTab.demo"
+            :style="currentTab.demoStyle">
+          </div>
+        </div>
+
+        <div class="code">
+          html code:
+          <pre><code class="html">{{ currentTab.demo }}</code></pre>
+          
+          <template v-if="currentTab.demoJS">
+            javascript code:
+            <pre><code class="javascript">{{ currentTab.demoJS }}</code></pre>
+          </template>
+        </div>
       </section>
     </div>
   </div>
@@ -39,29 +55,34 @@
 // 預先 import Highlight.js
 import hljs from "highlight.js/lib/core";
 import html from "highlight.js/lib/languages/xml";
+import javascript from "highlight.js/lib/languages/javascript";
 import "highlight.js/styles/vs2015.css";
 hljs.registerLanguage("html", html);
+hljs.registerLanguage("javascript", javascript);
 
-import { onMounted, watch, ref, computed, nextTick } from "vue";
+import { onMounted, watch, defineAsyncComponent, ref, computed, nextTick } from "vue";
 
 const tabs = [
   {
-    id: 'noteSpacing',
-    title: '字的間距',
+    id: "noteSpacing",
+    title: "字的間距",
     description: null,
     demoStyle: null,
+    component: null,
     demo: `
 <ul class="spacing-test">
   <li>I love bird. 我愛鳥</li>
   <li><span style="letter-spacing: 8px;">I love bird. 我愛鳥</span></li>
   <li><span style="word-spacing: 8px;">I love bird. 我愛鳥</span></li>
-</ul>`
+</ul>`,
+    demoJS: null
   },
   {
-    id: 'noteTextShadow',
-    title: '文字陰影',
+    id: "noteTextShadow",
+    title: "文字陰影",
     description: null,
     demoStyle: "font-size: 24px;",
+    component: null,
     demo: `
 <p style="text-shadow: 3px 3px 3px gray;">文字測試一</p>
 <p style="text-shadow: 3px 3px 3px black;">文字測試二</p>
@@ -71,13 +92,15 @@ const tabs = [
 <p style="text-shadow: 12px 5px 3px gray;">文字測試六</p>
 <p style="text-shadow: 12px 5px 10px gray;">文字測試七</p>
 <p style="text-shadow: 12px 5px 20px gray;">文字測試八</p>
-<p style="text-shadow: 12px 5px gray;">文字測試九</p>`
+<p style="text-shadow: 12px 5px gray;">文字測試九</p>`,
+    demoJS: null
   },
   {
-    id: 'noteBorderTest',
-    title: 'Border Test',
+    id: "noteBorderTest",
+    title: "Border Test",
     description: null,
     demoStyle: "display: flex; flex-wrap: wrap; gap: 12px;",
+    component: null,
     demo: `
 <div style="border: solid;">border: solid</div>
 <div style="border: double;">border: double</div>
@@ -87,75 +110,108 @@ const tabs = [
 <div style="border: groove;">border: groove</div>
 <div style="border: ridge;">border: ridge</div>
 <div style="border: inset;">border: inset</div>
-<div style="border: outset;">border: outset</div>`
+<div style="border: outset;">border: outset</div>`,
+    demoJS: null
   },
   {
-    id: 'noteFloat',
-    title: '文繞圖Float測試',
+    id: "noteFloat",
+    title: "文繞圖Float測試",
     description: null,
     demoStyle: "display: flex; flex-wrap: wrap;",
+    component: null,
     demo: `
-<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 300px; height: 200px;">
-  <div style="width: 100px; height: 67px; float: left; background-color: #c8c8c8; border: 1px solid #222222"></div>
-  <p style="padding: 20px;">測試文繞圖<br>（圖片style="float: left;"）</p>
+<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 400px; height: auto;">
+  <div style="width: 50%; aspect-ratio: 3/2; background-color: #ccc; border: 1px solid black; float: left;"></div>
+  <p>測試文繞圖<br>（圖片style="float: left;"）</p>
 </div>
-<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 300px; height: 200px;">
-  <a><img src="assets/Kodak_UltraMax_053.jpg" title="小門嶼一隅(Kodak Ultramax400)" style="width: 50%; float: right; padding: 15px;"></a>
-  <p style="padding: 20px;">測試文繞圖<br>（圖片style="float: right;"）</p>
+
+<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 400px; height: auto;">
+  <div style="width: 50%; aspect-ratio: 3/2; background-color: #ccc; border: 1px solid black; float: right;"></div>
+  <p>測試文繞圖<br>（圖片style="float: right;"）</p>
 </div>
-<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 300px; height: 200px;">
-  <a><img src="assets/Kodak_UltraMax_053.jpg" title="小門嶼一隅(Kodak Ultramax400)" style="width: 50%; padding: 15px;"></a>
-  <p style="padding: 20px; float: left;">測試文繞圖<br>（文字style="float: left;"）</p>
+
+<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 400px; height: auto;">
+  <div style="width: 50%; aspect-ratio: 3/2; background-color: #ccc; border: 1px solid black; float: right;"></div>
+  <p style="float: left;">測試文繞圖<br>（文字style="float: left;"）<br>（圖片亦需增加float: right）</p>
 </div>
-<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 300px; height: 200px;">
-  <a><img src="assets/Kodak_UltraMax_053.jpg" title="小門嶼一隅(Kodak Ultramax400)" style="width: 50%; padding: 15px;"></a>
-  <p style="padding: 20px; float: right;">測試文繞圖<br>（文字style="float: right;"）</p>
-</div>`
+
+<div style="background-color: #eeeeee; border: 0.5px solid gray; width: 400px; height: auto;">
+  <div style="width: 50%; aspect-ratio: 3/2; background-color: #ccc; border: 1px solid black; float: left;"></div>
+  <p style="float: right;">測試文繞圖<br>（文字style="float: right;"）<br>（圖片亦需增加float: left）</p>
+</div>`,
+    demoJS: null
   },
   {
-    id: 'noteDropDownHref',
-    title: '下拉式超連結',
+    id: "noteDropDownHref",
+    title: "下拉式超連結",
+    description: null,
+    demoStyle: null,
+    component: defineAsyncComponent(() =>
+      import("../../components/WebNoteView/WebNoteSimpleView/SearchEngine.vue")
+    ),
+    demo: `
+<select id="searchEngine">
+  <option value="Google">Google</option>
+  <option value="Yahoo">Yahoo!奇摩</option>
+  <option value="Bing">Bing</option>
+</select>
+<button onclick="toSearchEngine();">Go!</button>
+`,
+    demoJS: `
+// 下拉式超連結
+function toSearchEngine() {
+  var searchEngine = document.getElementById("searchEngine");
+  var selectedValue = searchEngine.value;
+
+  if (selectedValue === "Google") {
+    window.open("https://www.google.com.tw", "_blank");
+  } else if (selectedValue === "Yahoo") {
+    window.open("https://tw.yahoo.com", "");
+  } else if (selectedValue === "Bing") {
+    window.open("https://www.bing.com", "");
+  };  
+};`
+  },
+  {
+    id: "noteMoveToChangeImg",
+    title: "移動游標換照片",
     description: null,
     demoStyle: "font-size: 24px;",
+    component: null,
     demo: `
 <p style="text-shadow: 3px 3px 3px gray">文字測試一</p>
 <p style="text-shadow: 3px 3px 3px black">文字測試二</p>
-<p style="text-shadow: 12px 5px gray">文字測試九</p>`
+<p style="text-shadow: 12px 5px gray">文字測試九</p>`,
+    demoJS: null
   },
   {
-    id: 'noteMoveToChangeImg',
-    title: '移動游標換照片',
+    id: "notePictureChange",
+    title: "疊圖照片接替出現",
     description: null,
     demoStyle: "font-size: 24px;",
+    component: null,
     demo: `
 <p style="text-shadow: 3px 3px 3px gray">文字測試一</p>
 <p style="text-shadow: 3px 3px 3px black">文字測試二</p>
-<p style="text-shadow: 12px 5px gray">文字測試九</p>`
+<p style="text-shadow: 12px 5px gray">文字測試九</p>`,
+    demoJS: null
   },
   {
-    id: 'notePictureChange',
-    title: '疊圖照片接替出現',
+    id: "noteChineseFonts",
+    title: "各式中文字體",
     description: null,
     demoStyle: "font-size: 24px;",
+    component: null,
     demo: `
 <p style="text-shadow: 3px 3px 3px gray">文字測試一</p>
 <p style="text-shadow: 3px 3px 3px black">文字測試二</p>
-<p style="text-shadow: 12px 5px gray">文字測試九</p>`
-  },
-  {
-    id: 'noteChineseFonts',
-    title: '各式中文字體',
-    description: null,
-    demoStyle: "font-size: 24px;",
-    demo: `
-<p style="text-shadow: 3px 3px 3px gray">文字測試一</p>
-<p style="text-shadow: 3px 3px 3px black">文字測試二</p>
-<p style="text-shadow: 12px 5px gray">文字測試九</p>`
+<p style="text-shadow: 12px 5px gray">文字測試九</p>`,
+    demoJS: null
   },
   // ...把其他 tab 照此格式一一加入 tabs 陣列
 ];
 
-const activeTab = ref(tabs[3].id);
+const activeTab = ref(tabs[4].id);
 
 // 處理到這邊，後面待處理
 const currentTab = computed(() => {
@@ -231,6 +287,8 @@ pre {
   background: #2d2d2d;
   color: #f8f8f2;
   padding-bottom: 16px;
+  margin-top: 0;
+  font-size: 14px;
   overflow: auto;
   border-radius: 4px;
 }
